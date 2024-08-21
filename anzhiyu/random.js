bc32f5715f53bc5cb13dc4f9de83736848fc38fb
jiayihello <eryba218@gmail.com>
2024-08-21 03:24:36 +0000

    Update Post Draft _config.anzhiyu.yml by Qexo UTC 2024-08-21 Wednesday 03:24:36 Updated By Github Actions

---
+++ anzhiyu/random.js
@@ -0,0 +1,56 @@
+var posts=["2024/08/20/高志凯半岛电视台采访精华/","2024/08/10/Hexo搭建/","2024/06/19/verilog交通灯存档/","2024/06/19/quartus-ii-131及modelsim与modelsim-altera安装教程/","2024/05/07/silvaco2020安装教程/","2024/05/03/利用wireguard实现ipv6-only-vps使用双栈vps的v4出口获得ipv4访问/","2024/05/01/全能机器人框架----koishi----示例搭建-telegram-与-discord-互通机器人/","2024/05/01/搭建一个-cloudflare-workers-ai-telegram-机器人-免费且稳定-聊天amp画图/","2024/05/01/部署每日备份vps重要数据至阿里云盘/","2024/04/14/sing-boxhy2绕过校园网认证/","2024/04/09/linux利用rclone挂载云盘到本地/","2024/04/08/chevereto开心版搭建基于1panel/","2024/04/03/极简网页记事本/","2024/04/03/telegram-转发机器人-的安装步骤/","2024/04/03/pagermaid-pyro安装命令存档/","2024/04/01/通过cloudflaresaas回源优选ip使国内用户高速访问网站/","2024/02/06/用backblazecloudflare搭建免费图床/","2024/01/13/使用-cloudflare-workers-搭建自己的优选测速地址/","2024/01/13/优选ip脚本/","2024/01/13/自动更新-cloudflare-中的域名解析记录为最快-ipwindowslinux-脚本手动教程/","2024/01/13/cloudflare优选ip-自动ddns-shell脚本/","2024/01/13/爬取别人网站的优选反代ip/","2023/08/07/使用cdnfly自建cdn并配置cc防护/","2023/08/03/alpine系统常用命令/","2023/07/31/virtualizor开lxc-nat-vps并对接whmcs/","2023/07/31/修改备份/","2023/07/19/whmcs873开心版安装/","2023/07/16/免费版cloudflare防cc简单设置/","2023/07/15/订阅转换搭建修改版/","2023/07/14/订阅转换搭建原版前后端/","2023/07/14/nordvpn之wg搭建/","2023/07/13/sub-web订阅转换面板的搭建教程/","2023/07/12/在openvz虚拟化的vps安装bbr模块加速/","2023/07/11/oracle-甲骨文云2022年新版服务控制台更改密码邮箱租户名等教程/","2023/07/11/甲骨文更改登录账户教程旧版联盟账号/","2023/06/22/linux一键重装支持debian-12ubuntu-2204史上最强-转载/","2023/03/11/在vps中使用openvpn内置代理/","2023/02/14/分手模板收藏/","2023/02/13/甲骨文利用netbootxyz救砖重装任意系统/","2023/02/10/老毛子搭建v2/","2023/01/30/cloudflare-免费解析动态域名-ddns/","2023/01/28/扫描x-ui弱密码/","2023/01/27/windows-dd-linux/","2023/01/21/cloudflare-workers反代教程/","2023/01/16/通过origin-rules使cloudflare-cdn回源以使用任意的端口/","2023/01/16/cloudflare-zero-trust-申请/","2023/01/14/境内外多线部署cdn/","2023/01/14/代理搭建-koyeb篇/","2023/01/13/代理搭建-doprax篇/","2023/01/13/防止lxc小鸡pve环境中系统文件每次重启后自动修改/","2023/01/12/swap使用/","2023/01/11/海贼王壁纸/","2023/01/11/代理搭建-heroku篇/","2023/01/10/centos关闭防火墙/","2023/01/10/linux常用脚本/","2023/01/09/纯ipv6利用nat64访问ipv4/","2023/01/09/rustdesk自建远程桌面服务替代teamviewertodesk/","2023/01/06/一键dd纯净脚本/","2023/01/06/各类服务器空间收集/","2023/01/05/2023-年-01-月-05-日-13-时-21-分/","2023/01/05/百度云盘迁移文件到阿里云盘/","2023/01/05/arm框架搭建mtp代理/","2023/01/03/nat套cloudflare/","2023/01/03/使用tg机器人自动下载并上传频道内文件到本地amp网盘/","2023/01/03/通过udp53实现校园网免认证上网/","2023/01/03/telegram-bot-控制-aria2/","2023/01/03/利用rclone挂载网盘到linux/","2023/01/03/rclone常用命令参数/","2023/01/03/自建私有-api-挂载onedrive/","2023/01/03/rclone安装配置教程-连接onedrive/","2023/01/03/aria2rclone实现onedrive等网盘离线下载/","2023/01/03/b2创建以及套用cloudflare/","2023/01/03/linux利用gg实现国内访问github/","2023/01/02/vps常用测试脚本/","2023/01/01/站点声明/","2022/12/23/欢迎使用-typecho/"];function toRandomPost(){
+    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
+  };var friend_link_list=[{"name":"Hexo","link":"https://hexo.io/zh-tw/","avatar":"https://d33wubrfki0l68.cloudfront.net/6657ba50e702d84afb32fe846bed54fba1a77add/827ae/logo.svg","descr":"快速、简单且强大的网站框架"},{"name":"anzhiyu主题","link":"https://blog.anheyu.com/","avatar":"https://npm.elemecdn.com/anzhiyu-blog-static@1.0.4/img/avatar.jpg","descr":"生活明朗，万物可爱","siteshot":"https://npm.elemecdn.com/anzhiyu-theme-static@1.1.6/img/blog.anheyu.com.jpg"},{"name":"安知鱼","link":"https://blog.anheyu.com/","avatar":"https://npm.elemecdn.com/anzhiyu-blog-static@1.0.4/img/avatar.jpg","descr":"生活明朗，万物可爱","siteshot":"https://npm.elemecdn.com/anzhiyu-theme-static@1.1.6/img/blog.anheyu.com.jpg","color":"vip","tag":"技术"},{"name":"安知鱼","link":"https://blog.anheyu.com/","avatar":"https://npm.elemecdn.com/anzhiyu-blog-static@1.0.4/img/avatar.jpg","descr":"生活明朗，万物可爱","recommend":true}];
+    var refreshNum = 1;
+    function friendChainRandomTransmission() {
+      const randomIndex = Math.floor(Math.random() * friend_link_list.length);
+      const { name, link } = friend_link_list.splice(randomIndex, 1)[0];
+      Snackbar.show({
+        text:
+          "点击前往按钮进入随机一个友链，不保证跳转网站的安全性和可用性。本次随机到的是本站友链：「" + name + "」",
+        duration: 8000,
+        pos: "top-center",
+        actionText: "前往",
+        onActionClick: function (element) {
+          element.style.opacity = 0;
+          window.open(link, "_blank");
+        },
+      });
+    }
+    function addFriendLinksInFooter() {
+      var footerRandomFriendsBtn = document.getElementById("footer-random-friends-btn");
+      if(!footerRandomFriendsBtn) return;
+      footerRandomFriendsBtn.style.opacity = "0.2";
+      footerRandomFriendsBtn.style.transitionDuration = "0.3s";
+      footerRandomFriendsBtn.style.transform = "rotate(" + 360 * refreshNum++ + "deg)";
+      const finalLinkList = [];
+  
+      let count = 0;
+
+      while (friend_link_list.length && count < 3) {
+        const randomIndex = Math.floor(Math.random() * friend_link_list.length);
+        const { name, link, avatar } = friend_link_list.splice(randomIndex, 1)[0];
+  
+        finalLinkList.push({
+          name,
+          link,
+          avatar,
+        });
+        count++;
+      }
+  
+      let html = finalLinkList
+        .map(({ name, link }) => {
+          const returnInfo = "<a class='footer-item' href='" + link + "' target='_blank' rel='noopener nofollow'>" + name + "</a>"
+          return returnInfo;
+        })
+        .join("");
+  
+      html += "<a class='footer-item' href='/link/'>更多</a>";
+
+      document.getElementById("friend-links-in-footer").innerHTML = html;
+
+      setTimeout(()=>{
+        footerRandomFriendsBtn.style.opacity = "1";
+      }, 300)
+    };
